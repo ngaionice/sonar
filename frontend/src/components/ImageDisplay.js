369712dcdeedc6c62b9f5b0d913a3953f5197e8c
ngaionice <julian.myne@gmail.com>
2022-03-30 14:14:46 -0400

    Implement client-side deletion

--- frontend/src/components/ImageDisplay.js
+++ frontend/src/components/ImageDisplay.js
@@ -1,6 +1,28 @@
-import { ImageList, ImageListItem } from "@mui/material";
+import {
+  Button,
+  ButtonBase,
+  Dialog,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
+
+import CheckIcon from "@mui/icons-material/Check";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+import { useUser } from "../contexts/userContext";
 
 function ImageDisplay({ images }) {
+  const [user] = useUser();
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [displayed, setDisplayed] = useState(null);
+  const [displayIcon, setDisplayIcon] = useState(<ContentCopyIcon />);
+
   const ListEntry = ({ image }) => {
     const { id: title, url: img } = image;
 
@@ -12,19 +34,81 @@ function ImageDisplay({ images }) {
     const srcUrl = img + extension + urlModifiers;
     const srcSetUrl = img + extension + urlModifiers + urlSetModifiers;
 
+    const handleClick = () => {
+      setDisplayed(image);
+      setDialogOpen(true);
+    };
+
     return (
       <ImageListItem>
-        <img src={srcUrl} srcSet={srcSetUrl} alt={title} loading="lazy" />
+        <ButtonBase onClick={handleClick}>
+          <img src={srcUrl} srcSet={srcSetUrl} alt={title} loading="lazy" />
+        </ButtonBase>
       </ImageListItem>
     );
   };
 
+  const UrlDisplay = () => {
+    if (!displayed?.url) return null;
+    const url = displayed.url;
+
+    const handleClick = () => {
+      navigator.clipboard.writeText(url).then(() => {
+        setDisplayIcon(<CheckIcon />);
+        setTimeout(() => setDisplayIcon(<ContentCopyIcon />), 2000);
+      });
+    };
+
+    return (
+      <Stack direction="row" spacing={1} alignItems="center">
+        <TextField disabled value={url} size="small" />
+        <IconButton onClick={handleClick}>{displayIcon}</IconButton>
+      </Stack>
+    );
+  };
+
+  const DeleteButton = () => {
+    if (!displayed?.id || isNaN(displayed?.index)) return null;
+    const id = displayed.id;
+
+    const handleClick = () => {
+      axios
+        .delete("http://localhost:8000/api/files/delete", {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+          params: {
+            keys: JSON.stringify([id]),
+          },
+        })
+        .then(() => {
+          images.splice(displayed.index, 1);
+          handleClose();
+        });
+    };
+
+    return <Button onClick={handleClick}>Delete</Button>;
+  };
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+
   return (
-    <ImageList variant="masonry" cols={3} gap={8}>
-      {images.map((image) => {
-        return <ListEntry key={image.id} image={image} />;
-      })}
-    </ImageList>
+    <>
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {images.map((image, index) => {
+          image.index = index;
+          return <ListEntry key={image.id} image={image} />;
+        })}
+      </ImageList>
+      <Dialog open={dialogOpen} onClose={handleClose}>
+        <Stack spacing={1}>
+          <UrlDisplay />
+          <DeleteButton />
+        </Stack>
+      </Dialog>
+    </>
   );
 }
 
