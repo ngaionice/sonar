3e4bf41c88f50ef452ce0b13c5e1a58b4467a2a7
ngaionice <julian.myne@gmail.com>
2022-03-30 17:48:19 -0400

    Implement file fetching via URL, refactor server file handling code

--- backend/controllers/file-controller.js
+++ backend/controllers/file-controller.js
@@ -2,30 +2,97 @@ import asyncHandler from "express-async-handler";
 import * as Imgur from "../storage/imgur-manager.js";
 import * as AWS from "../storage/aws-manager.js";
 import * as File from "../database/file.js";
+import axios from "axios";
+
+const getTags = (tagsString) => {
+  let tags;
+  if (tagsString) {
+    const temp = JSON.parse(tagsString);
+    tags = Array.isArray(temp) ? temp : [];
+  } else {
+    tags = [];
+  }
+  if (tags.length < 1) {
+    tags.push("untagged");
+  }
+  return tags;
+};
+
+const getNewFilename = (filename) =>
+  String(Math.floor(Date.now() / 1000)).concat("-", filename ?? "autoname");
+
+const isValidFiletype = (type) => {
+  const allowedTypesPrefix = /image\//;
+  return type.search(allowedTypesPrefix) !== 0;
+};
+
+const getDataFromUpload = (req, res) => {
+  const { buffer, size, mimetype, originalname: oldName } = req.file;
+  const { tags: tagsString, isPublic } = req.body;
+
+  if (!isValidFiletype(mimetype)) {
+    res.status(400).send("Invalid file type");
+    throw new Error("Invalid file type");
+  }
+
+  if (size > 15728640) {
+    res.status(400).send("File too large");
+    throw new Error("File too large");
+  }
+
+  const tags = getTags(tagsString);
+  const name = getNewFilename(oldName);
+  return {
+    buffer,
+    name,
+    tags,
+    isPublic,
+  };
+};
+
+const getDataFromUrl = async (req, res) => {
+  const { tags: tagsString, isPublic, srcUrl } = req.body;
+  const tags = getTags(tagsString);
+
+  const image = await axios.get(srcUrl, {
+    responseType: "arraybuffer",
+    timeout: 30000, // times out after 30s
+  });
+  if (!isValidFiletype(image.headers["content-type"])) {
+    res.status(400).send("Invalid file type");
+    throw new Error("Invalid file type");
+  }
+  if (image.data.byteLength > 15728640) {
+    res.status(400).send("File too large");
+    throw new Error("File too large");
+  }
+
+  const buffer = Buffer.from(image.data, "utf-8");
+
+  const name = getNewFilename();
+
+  return {
+    buffer,
+    name,
+    tags,
+    isPublic,
+  };
+};
 
 const upload = (dbClient) =>
   asyncHandler(async (req, res) => {
-    const { buffer, size, mimetype, originalname: oldName } = req.file;
-    const { tags: tagsString, isPublic } = req.body;
-    const tags = tagsString ? JSON.parse(tagsString) : [];
-    if (tags.length < 1) {
-      tags.push("untagged");
-    }
-
-    const allowedTypesPrefix = /image\//;
-    if (mimetype.search(allowedTypesPrefix) !== 0) {
-      res.status(400).send("Invalid file type");
+    let data;
+    try {
+      if (req.file) data = getDataFromUpload(req, res);
+      else data = await getDataFromUrl(req, res);
+    } catch (e) {
+      console.log("Failed data extraction.");
       return;
     }
-
-    if (size > 15728640) {
-      res.status(400).send("File too large");
-      return;
-    }
+    const { buffer, name, tags, isPublic } = data;
 
     const out = {};
 
-    const name = String(Math.floor(Date.now() / 1000)).concat("-", oldName);
     const { key, url: awsUrl } = await AWS.upload(buffer, name);
     out.aws = awsUrl;
 
