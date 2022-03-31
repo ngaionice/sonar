e140ad46733bcb588eb70ac026d8edee3e18ed15
ngaionice <julian.myne@gmail.com>
2022-03-30 21:38:40 -0400

    Refactor server handling code some more, fix logic issue
    Fix file type detection logic, sign was flipped

--- backend/controllers/file-controller.js
+++ backend/controllers/file-controller.js
@@ -2,82 +2,10 @@ import asyncHandler from "express-async-handler";
 import * as Imgur from "../storage/imgur-manager.js";
 import * as AWS from "../storage/aws-manager.js";
 import * as File from "../database/file.js";
-import axios from "axios";
-
-const getTags = (tagsString) => {
-  let tags;
-  if (tagsString) {
-    const temp = JSON.parse(tagsString);
-    tags = Array.isArray(temp) ? temp : [];
-  } else {
-    tags = [];
-  }
-  if (tags.length < 1) {
-    tags.push("untagged");
-  }
-  return tags;
-};
-
-const getNewFilename = (filename) =>
-  String(Math.floor(Date.now() / 1000)).concat("-", filename ?? "autoname");
-
-const isValidFiletype = (type) => {
-  const allowedTypesPrefix = /image\//;
-  return type.search(allowedTypesPrefix) !== 0;
-};
-
-const getDataFromUpload = (req, res) => {
-  const { buffer, size, mimetype, originalname: oldName } = req.file;
-  const { tags: tagsString, isPublic } = req.body;
-
-  if (!isValidFiletype(mimetype)) {
-    res.status(400).send("Invalid file type");
-    throw new Error("Invalid file type");
-  }
-
-  if (size > 15728640) {
-    res.status(400).send("File too large");
-    throw new Error("File too large");
-  }
-
-  const tags = getTags(tagsString);
-  const name = getNewFilename(oldName);
-  return {
-    buffer,
-    name,
-    tags,
-    isPublic,
-  };
-};
-
-const getDataFromUrl = async (req, res) => {
-  const { tags: tagsString, isPublic, srcUrl } = req.body;
-  const tags = getTags(tagsString);
-
-  const image = await axios.get(srcUrl, {
-    responseType: "arraybuffer",
-    timeout: 30000, // times out after 30s
-  });
-  if (!isValidFiletype(image.headers["content-type"])) {
-    res.status(400).send("Invalid file type");
-    throw new Error("Invalid file type");
-  }
-  if (image.data.byteLength > 15728640) {
-    res.status(400).send("File too large");
-    throw new Error("File too large");
-  }
-
-  const buffer = Buffer.from(image.data, "utf-8");
-
-  const name = getNewFilename();
-
-  return {
-    buffer,
-    name,
-    tags,
-    isPublic,
-  };
-};
+import {
+  getDataFromUpload,
+  getDataFromUrl,
+} from "./file-controller-helpers.js";
 
 const upload = (dbClient) =>
   asyncHandler(async (req, res) => {
